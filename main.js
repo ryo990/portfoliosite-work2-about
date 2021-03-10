e827fb18544b601eda3fa35cb20bd71a0201adcd
ryo990 <79450862+ryo990@users.noreply.github.com>
2021-03-10 19:04:28 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const pics_src = ["image/cafe1.jpg","image/cafe2.jpg"];
+let num = -1;
+
+function slideshow_timer(){
+  if (num === 1){
+  num = 0;
+  } 
+  else {
+  num ++;
+  }
+  document.getElementById("mypic").src = pics_src[num];
+}
+
+setInterval(slideshow_timer, 3000);
